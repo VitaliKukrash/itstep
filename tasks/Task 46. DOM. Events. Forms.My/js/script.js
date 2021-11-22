67aa1c007fe477fb664e9986692f95ff571bb311
VitaliKukrash <koresh007@tut.by>
2021-11-22 21:20:07 +0300

    task46

--- tasks/Task 46. DOM. Events. Forms.My/js/script.js
+++ tasks/Task 46. DOM. Events. Forms.My/js/script.js
@@ -8,7 +8,7 @@
 input.onblur = function() {
       this.classList.remove('focused');
     }
-*/
+
 
 let form = document.forms.my;
 
@@ -26,3 +26,24 @@ document.forms[0].addEventListener(('focusout'), () => elem.classList.remove('fo
 // - во втором поле введено не число
 // - в третьем поле введен не email  
 
+//5. Дано форма с полями:
+//    - имя
+//    - email
+//    - выбор пола (радиокнопки)
+//    - выпадающий языками программирования
+//    - "Я не робот" (чекбокс)
+// Если пользователь заполнит все данные, то необоходимо скрыть (не удалить) форму и отобразить эти данные.
+// Если хотя бы одно поле не заполнено или заполнено некорректно, то нужно под полем вывести ошибку об этом.
+
+let form1 = document.forms.form5;
+
+let elem1 = form1.elements.email;
+
+console.log(elem1);*/
+
+document.forms.reg_form.onsubmit = function(event){
+  let FIO = this.FIO.value;
+  console.log(FIO);
+
+  return false;
+}
